efc7c0be9654e9a3385608c9d39b37fa15d89d1e
Kimberly Luke <kimberly_luke@cable.comcast.com>
2021-07-01 16:14:08 -0400

    Finished 12+ Tests

--- src/test/java/BalancedBracketsTests.java
+++ src/test/java/BalancedBracketsTests.java
@@ -13,4 +13,70 @@ public class BalancedBracketsTests {
     public void testBasicBrackets() {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void testDoubleBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[]]"));
+    }
+
+    @Test
+    public void testTripleBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[]]]"));
+    }
+
+    @Test
+    public void testWordInBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
+
+    @Test
+    public void testPartWordInBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("Launch[Code]"));
+    }
+
+    @Test
+    public void testWordAfterBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]LaunchCode"));
+    }
+
+    @Test
+    public void testWordBeforeBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("LaunchCode[]"));
+    }
+
+    @Test
+    public void testOpenBracketNotClosed() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void testClosedBracketNotOpened() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void testTwoOpenBracketsNotClosed() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[["));
+    }
+
+    @Test
+    public void testTwoClosedBracketsNotOpened() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]]"));
+    }
+
+    @Test
+    public void testBracketClosedWithBasicBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]"));
+    }
+
+    @Test
+    public void testBracketOpenedWithBasicBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+
 }
